bf176fe35e8cc5c8a32b28642d9877ce23c70196
Rodrigo Nieva <nievarodrigo40@gmail.com>
2026-08-24 21:58:29 -0300

    fix: remove unfinished projects from portfolio

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,7 +20,6 @@ const blog = defineCollection({
       canonicalURL: z.string().optional(),
       hideEditPost: z.boolean().optional(),
       timezone: z.string().optional(),
-      repoURL: z.string().url().optional(),
     }),
 });
 
